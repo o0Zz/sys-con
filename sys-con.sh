bc39b28e32b8a66c267c72714d129560925cc778
o0Zz <oozz01@gmail.com>
2025-09-03 22:33:09 +0200

    Refacto script to make it faster and simpler

--- sys-con.sh
+++ sys-con.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Script dependencies
+# ---------------------
+# pacman -S lftp
 #
 # This script is used to simplify nintendo switch developement
 # You need sys-ftpd installed
@@ -21,39 +24,31 @@ NRO_FILE=./out/switch/sys-con.nro
 DST_NRO_FILE=switch/sys-con.nro
 LOG_FILE=config/sys-con/log.txt
 REPORT_FOLDER=atmosphere/fatal_errors
+DMP_FOLDER=config/sys-con
 
 usage () {
   echo "Usage: "
-  echo "       $0 ftp upload"
-  echo "       $0 ftp logs"
-  echo "       $0 ftp crash"
-  echo "       $0 sd upload"
-  echo "       $0 sd logs"
-  echo "       $0 sd crash"
+  echo "       $0 ftp <upload|logs|crash|dmp>"
+  echo "       $0 sd <upload|logs|crash|dmp>"
   echo "       $0 build"
   echo "       $0 stacktrace"
   exit 1
 }
 
 display_logs () {
-	BLUE='\033[0;34m'
-	RED='\033[0;31m'
-	YELLOW='\033[0;33m'
-	NC='\033[0m'
-
 	echo "---- LOGS ----"
 	while IFS= read -r line
 	do
 		if [[ $line == "|I|"* ]]; then
-			echo -e "${BLUE}${line}${NC}"
+			echo -e "\033[0;34m${line}\033[0m"
 		elif [[ $line == "|E|"* ]]; then
-			echo -e "${RED}${line}${NC}"
+			echo -e "\033[0;31m${line}\033[0m"
 		elif [[ $line == "|W|"* ]]; then
-			echo -e "${YELLOW}${line}${NC}"
+			echo -e "\033[0;33m${line}\033[0m"
 		else
 			echo "$line"
 		fi
-	done < ./log.txt
+	done < $1
     echo "---- END ----"
 }
 
@@ -61,91 +56,91 @@ display_fatalerror () {
 	if [ ! -f "AFE_Parser.exe" ]; then
 		curl -L -o AFE_Parser.exe https://github.com/o0Zz/AFE_Parser/releases/download/v1.3.1/AFE_Parser.exe
 	fi
-	./AFE_Parser.exe -report fatal_error.bin -elf $ELF_FILE
+	if [ ! $1 ]; then
+		echo "*** No crash report ***"
+		exit 1
+	fi
+	./AFE_Parser.exe -report $1 -elf $ELF_FILE
 }
+
 
 if [ "$1" == "ftp" ]; then
     if [ "$2" == "upload" ]; then
         echo "FTP upload"
         curl -u "$FTP_USER:$FTP_PASS" -T $NSP_FILE "$FTP_URL/$DST_NSP_FILE"  || exit 1
-        curl -u "$FTP_USER:$FTP_PASS" -T $NRO_FILE "$FTP_URL/$DST_NRO_FILE"  || exit 1
-        exit 0
-    fi
-	
-	if [ "$2" == "logs" ]; then
+        #curl -u "$FTP_USER:$FTP_PASS" -T $NRO_FILE "$FTP_URL/$DST_NRO_FILE"  || exit 1
+    elif [ "$2" == "logs" ]; then
         curl -u "$FTP_USER:$FTP_PASS" "$FTP_URL/$LOG_FILE" -o ./log.txt || exit 1
-        display_logs
-		exit 0
-    fi
+        display_logs ./log.txt
+    elif  [ "$2" == "crash" ]; then
+		echo "Downloading all .bin crash reports from $REPORT_FOLDER..."
+		
+		rm *.bin
+		lftp -u "$FTP_USER","$FTP_PASS" "$FTP_URL" <<EOF
+cd $REPORT_FOLDER
+mget *.bin -O .
+glob -a rm *.bin
+bye
+EOF
+
+        display_fatalerror $(ls | grep -Eo '[^ ]+\.bin' | head -n 1)
+    elif  [ "$2" == "dmp" ]; then
+		echo "Downloading and deleting all .dmp files from $DMP_FOLDER..."
+		
+		rm *.dmp
+		lftp -u "$FTP_USER","$FTP_PASS" "$FTP_URL" <<EOF
+cd $DMP_FOLDER
+mget *.dmp -O .
+glob -a rm *.dmp
+bye
+EOF
+
+	else
+		usage
+	fi
 	
-	if [ "$2" == "crash" ]; then
-        echo "Retrieving report file list..."
-        # List all .bin files in the crash report folder
-        first_file=$(curl -s -u "$FTP_USER:$FTP_PASS" "$FTP_URL/$REPORT_FOLDER/" | grep -Eo '[^ ]+\.bin' | head -n 1)
-        echo "Retrieving report file: $first_file"
-        curl -u "$FTP_USER:$FTP_PASS" "$FTP_URL/$REPORT_FOLDER/$first_file" -o ./fatal_error.bin || exit 1
-
-        display_fatalerror
-
-        if [ -n "$first_file" ]; then
-            echo "Deleting remote crash report..."
-            curl -s -u "$FTP_USER:$FTP_PASS" -Q "DELE $REPORT_FOLDER/$first_file" "$FTP_URL" >/dev/null 2>&1
-        fi
-        
-		exit 0
-    fi
-fi
-
-if [ "$1" == "sd" ]; then
+elif  [ "$1" == "sd" ]; then
     if [ "$2" == "upload" ]; then
         echo "SD card copy"
         cp  $NSP_FILE /d/$DST_NSP_FILE || exit 1
+		#cp  $NRO_FILE /d/$DST_NRO_FILE || exit 1
         sync
         echo "OK"
-        exit 0  
-    fi
-	
-	if [ "$2" == "logs" ]; then
+
+    elif [ "$2" == "logs" ]; then
         cp  /d/$LOG_FILE ./log.txt || exit 1
-        display_logs
-		exit 0
+        display_logs ./log.txt
+
+    elif [ "$2" == "crash" ]; then
+		echo "Downloading all .bin crash reports from $REPORT_FOLDER..."
+		rm *.bin
+		cp /d/$REPORT_FOLDER/*.bin .
+		rm /d/$REPORT_FOLDER/*.bin
+		
+        display_fatalerror $(ls | grep -Eo '[^ ]+\.bin' | head -n 1)
+	elif  [ "$2" == "dmp" ]; then
+		echo "Downloading and deleting all .dmp files from $DMP_FOLDER..."
+		
+		rm *.dmp
+		cp /d/$DMP_FOLDER/*.dmp .
+
+	else
+		usage
     fi
 	
-	if [ "$2" == "crash" ]; then
-        first_file=$(ls /d/$REPORT_FOLDER/*.bin 2>/dev/null | head -n 1)
-		echo "Retriving report file: '$first_file'"
-		cp "$first_file" ./fatal_error.bin
-		
-		display_fatalerror
-        
-        if [ -n "$first_file" ]; then
-            echo "Deleting crash reports..."
-            rm /d/$REPORT_FOLDER/*.bin
-        fi
-
-		exit 0
-    fi
-fi
-
-if [ "$1" == "build" ]; then
+elif [ "$1" == "build" ]; then
 	rm *.zip
 	make distclean || exit 1
-	exit 0
-fi
-
-if [ "$1" == "stacktrace" ]; then
+	
+elif [ "$1" == "stacktrace" ]; then
     echo "PC: XXXXXXX value ?"
     read pc_value
 	
     echo "Backtrace - Start Address: XXXXXXX value ?"
     read backtrace_value
 
-    # Perform arithmetic subtraction correctly
-    ret=$(printf "0x%X\n" $((0x$pc_value - 0x$backtrace_value)))
-	echo "$ret"
-	
-    # Use the computed address in addr2line
-    /opt/devkitpro/devkitA64/bin/aarch64-none-elf-addr2line.exe -e "$ELF_FILE" -f -p -C -a "$ret"
-    exit 0
+    addr=$(printf "0x%X\n" $((0x$pc_value - 0x$backtrace_value)))
+    /opt/devkitpro/devkitA64/bin/aarch64-none-elf-addr2line.exe -e "$ELF_FILE" -f -p -C -a "$addr"
+else
+	usage
 fi
-usage
